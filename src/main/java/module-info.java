fcc119403518b39a3a851a36d73be9403f9a5b49
RobertVxV <robertvinitchi@gmail.com>
2023-03-20 17:21:11 +0200

    SM-1 Added the login screen

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,8 @@
 module com.example.studentmanager {
     requires javafx.controls;
     requires javafx.fxml;
-            
-                            
-    opens com.example.studentmanager to javafx.fxml;
-    exports com.example.studentmanager;
+
+
+    opens oose.oose.fis.lab.student.manager to javafx.fxml;
+    exports oose.oose.fis.lab.student.manager;
 }
--- src/main/java/oose/oose/fis/lab/student/manager/HelloController.java
+++ src/main/java/oose/oose/fis/lab/student/manager/HelloController.java
@@ -1,4 +1,4 @@
-package com.example.studentmanager;
+package oose.oose.fis.lab.student.manager;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
